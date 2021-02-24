0b35672ac7704d74a109a21e53fe89ea3d6c2a6d
Amanda Paranamana <neyomi22@hotmail.com>
2021-02-24 16:51:55 +1100

    Completed the Controller and View functionality

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   
   validates :content, presence: true
-  validates :rating, numericality: true,  
-   inclusion: { in: 0..5 }
+  validates :rating, numericality: { only_integer: true }, :inclusion => (0..5).to_a
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+pizzeria = { name: "Pizzeria", address: "7, Clyde North, VIC ", phone_number: "0454 575 555", category: "italian" }
+lucky_chan = { name: "Lucky chan", address: "14, Flinder street, VIC", phone_number: "0444 457 345", category: "chinese" }
+la_cafe = { name: "La cafe", address: "17, Glen waverely", phone_number: "0458 345 687", category: "french" }
+yukimao = { name: "Yukimao", address: "18, Clayton, VIC", phone_number: "0458 678 495", category: "japanese" }
+waffles = { name: "Waffles", address: "20, Richmond, VIC", phone_number: "0425 458 585", category: "belgian" }
+
+[ pizzeria, lucky_chan, la_cafe, yukimao, waffles ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
